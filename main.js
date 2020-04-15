27c26764b2c09417cee5fb7640af637b0052b7a5
jravas <josip.ravas.broj@gmail.com>
2020-04-15 23:22:42 +0200

    Service worker test

---
+++ main.js
@@ -0,0 +1,39 @@
+Notification.requestPermission();
+const init = async () => {
+    if (!'serviceWorker' in navigator) {
+        console.log('Service workers are not supported.');
+        return
+    }
+
+    try {
+        const registration = await navigator.serviceWorker.register('/sw.js')
+        registration.addEventListener('updatefound', () => {
+            console.log('A new service worker is being installed:',
+                registration.installing);
+        });
+    } catch (e) {
+        console.error('Service worker registration failed:', e);
+        return
+    }
+
+    const isReady = await navigator.serviceWorker.ready
+    console.log(isReady)
+    sendMessage('Service worker ready and message recived')
+}
+
+init()
+
+const sendMessage = (message) => {
+    if (!'serviceWorker' in navigator) {
+        console.log('Service workers are not supported.');
+        return
+    }
+    if (!navigator.serviceWorker.controller) {
+        console.log('No controller');
+        return
+    }
+    navigator.serviceWorker.controller.postMessage({
+        type: 'MESSAGE',
+        payload: message
+    });
+}
